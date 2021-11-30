101703188e8975c287652ee52e4a2dd24d6b5940
Janahan Selvanayagam <JSelvanayagam@robarts.ca>
2021-11-30 13:29:25 -0500

    add-concat-script

--- simianpy/scripts/util/__init__.py
+++ simianpy/scripts/util/__init__.py
@@ -1,5 +1,6 @@
 from .cmr import cmr
 from .write_to_sound import write_to_sound
+from .concat import concat
 
 import click
 
@@ -9,3 +10,4 @@ def util():
 
 util.add_command(cmr)
 util.add_command(write_to_sound)
+util.add_command(concat)
