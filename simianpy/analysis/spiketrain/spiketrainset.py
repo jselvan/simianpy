1acc3afcb987d9007764efd59f7414818c842f76
Janahan Selvanayagam <seljanahan@hotmail.com>
2025-05-23 19:25:07 +0100

    minor fixes to spiketrainset

--- simianpy/analysis/spiketrain/spiketrainset.py
+++ simianpy/analysis/spiketrain/spiketrainset.py
@@ -1,17 +1,19 @@
 from typing import Optional, Sequence, Tuple
+import zipfile
 
 import numpy as np
 from numpy.typing import ArrayLike
 import pandas as pd
 import xarray as xr
-import zipfile
+
 
 class SpikeTrainSet:
-    def __init__(self, 
-        trialids: np.ndarray, 
-        unitids: np.ndarray, 
-        spike_times: np.ndarray, 
-        window: Optional[Tuple[float, float]]=None
+    def __init__(
+        self,
+        trialids: np.ndarray,
+        unitids: np.ndarray,
+        spike_times: np.ndarray,
+        window: Optional[Tuple[float, float]] = None,
     ):
         """Initialize SpikeTrainSet
 
@@ -45,12 +47,13 @@ class SpikeTrainSet:
             raise ValueError("All inputs must be the same length")
 
     @classmethod
-    def from_arrays(cls, 
-        spike_timestamps: ArrayLike, 
-        event_timestamps: ArrayLike, 
-        window: Tuple[float, float], 
-        spike_labels: Optional[ArrayLike]=None, 
-        event_labels: Optional[ArrayLike]=None
+    def from_arrays(
+        cls,
+        spike_timestamps: ArrayLike,
+        event_timestamps: ArrayLike,
+        window: Tuple[float, float],
+        spike_labels: Optional[ArrayLike] = None,
+        event_labels: Optional[ArrayLike] = None,
     ) -> "SpikeTrainSet":
         """Generate SpikeTrainSet from arrays of spike and event timestamps
 
@@ -66,7 +69,7 @@ class SpikeTrainSet:
             Array of spike labels
         event_labels : array_like or None
             Array of event labels
-        
+
         Returns
         -------
         SpikeTrainSet
@@ -89,9 +92,9 @@ class SpikeTrainSet:
         event_timestamps = event_timestamps[evt_sort_idx]
         event_labels = event_labels[evt_sort_idx]
 
-        left, right = event_timestamps+window[0], event_timestamps+window[1]
-        lidx = np.searchsorted(spike_timestamps, left, side='left')
-        ridx = np.searchsorted(spike_timestamps, right, side='right')
+        left, right = event_timestamps + window[0], event_timestamps + window[1]
+        lidx = np.searchsorted(spike_timestamps, left, side="left")
+        ridx = np.searchsorted(spike_timestamps, right, side="right")
         lengths = ridx - lidx
         offset = np.repeat(event_timestamps, lengths)
         event_labels = np.repeat(event_labels, lengths)
@@ -102,11 +105,12 @@ class SpikeTrainSet:
         return cls(event_labels, spike_labels_aligned, spike_times_aligned, window)
 
     @classmethod
-    def from_series(cls, 
-            spike_timestamps: pd.Series, 
-            event_timestamps: pd.Series, 
-            window: Tuple[float, float]
-        ):
+    def from_series(
+        cls,
+        spike_timestamps: pd.Series,
+        event_timestamps: pd.Series,
+        window: Tuple[float, float],
+    ):
         """Generate SpikeTrainSet from pd.Series of spike and event timestamps
 
         Parameters
@@ -123,13 +127,13 @@ class SpikeTrainSet:
         SpikeTrainSet
         """
         return cls.from_arrays(
-            np.asarray(spike_timestamps.values), 
-            np.asarray(event_timestamps.values), 
-            window, 
-            spike_timestamps.index, 
-            event_timestamps.index
+            np.asarray(spike_timestamps.values),
+            np.asarray(event_timestamps.values),
+            window,
+            spike_timestamps.index,
+            event_timestamps.index,
         )
-    
+
     def to_dataframe(self):
         """Convert spike train set to DataFrame
 
@@ -138,13 +142,17 @@ class SpikeTrainSet:
         df : pd.DataFrame
             DataFrame with columns 'trialid', 'unitid', 'spike_time'
         """
-        return pd.DataFrame({
-            'trialid': self.trialids,
-            'unitid': self.unitids,
-            'spike_time': self.spike_times
-        })
-        
-    def to_psth(self, time_bins: Optional[Sequence]=None, time_step: Optional[float]=None) -> xr.DataArray:
+        return pd.DataFrame(
+            {
+                "trialid": self.trialids,
+                "unitid": self.unitids,
+                "spike_time": self.spike_times,
+            }
+        )
+
+    def to_psth(
+        self, time_bins: Optional[Sequence] = None, time_step: Optional[float] = None
+    ) -> xr.DataArray:
         """Convert spike train set to PSTH matrix
 
         Parameters
@@ -158,7 +166,7 @@ class SpikeTrainSet:
         -------
         psth : xarray.DataArray
             PSTH matrix with dimensions 'trialid', 'unitid', 'time'
-        
+
         Raises
         ------
         ValueError
@@ -171,38 +179,40 @@ class SpikeTrainSet:
             time_bin_array = np.asarray(time_bins)
         else:
             left, right = self.window
-            left = left-(time_step/2)
-            right = right+time_step
+            left = left - (time_step / 2)
+            right = right + time_step
             time_bin_array = np.arange(left, right, time_step)
-        
-        trialids_unique, trialids_digitized = np.unique(self.trialids, return_inverse=True)
+
+        trialids_unique, trialids_digitized = np.unique(
+            self.trialids, return_inverse=True
+        )
         unitids_unique, unitids_digitized = np.unique(self.unitids, return_inverse=True)
         psth, _ = np.histogramdd(
             np.array([trialids_digitized, unitids_digitized, self.spike_times]).T,
             bins=[
                 np.arange(trialids_unique.size + 1),
                 np.arange(unitids_unique.size + 1),
-                time_bin_array
-            ]
+                time_bin_array,
+            ],
         )
         time = time_bin_array[:-1] + (time_bin_array[1] - time_bin_array[0]) / 2
         psth = xr.DataArray(
             psth,
-            coords={
-                'trialid': trialids_unique,
-                'unitid': unitids_unique,
-                'time': time
-            },
-            dims=['trialid', 'unitid', 'time']
+            coords={"trialid": trialids_unique, "unitid": unitids_unique, "time": time},
+            dims=["trialid", "unitid", "time"],
         )
         return psth
 
-    def get_firing_rates(self, window: Optional[Tuple[float, float]]=None) -> xr.DataArray:
+    def get_firing_rates(
+        self, window: Optional[Tuple[float, float]] = None
+    ) -> xr.DataArray:
         if window is None:
             window = self.window
         # check that the window is within the range of the spike times
         if window[0] < self.window[0] or window[1] > self.window[1]:
-            raise ValueError("Window must be within the range of the provided SpikeTrainSet")
+            raise ValueError(
+                "Window must be within the range of the provided SpikeTrainSet"
+            )
         duration = window[1] - window[0]
         fr = self.to_psth(time_bins=window) / duration
         return fr
@@ -212,34 +222,34 @@ class SpikeTrainSet:
 
     def save(self, path: str):
         """Save the SpikeTrainSet as a DataArray in a zip file."""
-        with zipfile.ZipFile(path, 'w') as zf:
+        with zipfile.ZipFile(path, "w") as zf:
             # save self.spike_times, self.trialids and self.unitids
-            # as npy files within this archive 
-            with zf.open('spike_times.npy', 'w') as f:
+            # as npy files within this archive
+            with zf.open("spike_times.npy", "w") as f:
                 np.save(f, self.spike_times)
-            with zf.open('trialids.npy', 'w') as f:
+            with zf.open("trialids.npy", "w") as f:
                 np.save(f, self.trialids)
-            with zf.open('unitids.npy', 'w') as f:
+            with zf.open("unitids.npy", "w") as f:
                 np.save(f, self.unitids)
             # save the window as a text file
-            with zf.open('window.txt', 'w') as f:
+            with zf.open("window.txt", "w") as f:
                 value = f"{self.window[0]} {self.window[1]}"
-                f.write(value.encode('utf-8'))
+                f.write(value.encode("utf-8"))
 
     @classmethod
     def load(cls, path: str) -> "SpikeTrainSet":
-        with zipfile.ZipFile(path, 'r') as zf:
+        with zipfile.ZipFile(path, "r") as zf:
             # load self.spike_times, self.trialids and self.unitids
-            # from npy files within this archive 
-            with zf.open('spike_times.npy') as f:
+            # from npy files within this archive
+            with zf.open("spike_times.npy") as f:
                 spike_times = np.load(f)
-            with zf.open('trialids.npy') as f:
+            with zf.open("trialids.npy") as f:
                 trialids = np.load(f)
-            with zf.open('unitids.npy') as f:
+            with zf.open("unitids.npy") as f:
                 unitids = np.load(f)
             # load the window from a text file
-            with zf.open('window.txt') as f:
-                window = tuple(map(float, f.read().decode('utf-8').split()))
+            with zf.open("window.txt") as f:
+                window = tuple(map(float, f.read().decode("utf-8").split()))
                 if len(window) != 2:
                     raise ValueError("Window must be a tuple of length 2")
         return cls(trialids, unitids, spike_times, window)
@@ -248,9 +258,13 @@ class SpikeTrainSet:
         try:
             from PyQt5.QtCore import Qt
             from PyQt5.QtWidgets import QApplication, QMainWindow
-            from simianpy.analysis.spiketrain.spiketrainsetviewer import SpikeTrainSetViewer
+            from simianpy.analysis.spiketrain.spiketrainsetviewer import (
+                SpikeTrainSetViewer,
+            )
         except ImportError:
-            raise ImportError("pyqtmgl is not set up. Please install it to use this function.")
+            raise ImportError(
+                "pyqtmgl is not set up. Please install it to use this function."
+            )
         QApplication.setAttribute(Qt.AA_ShareOpenGLContexts, True)
         app = QApplication([])
         window = QMainWindow()
